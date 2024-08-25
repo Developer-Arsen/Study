1a05013419f0013f62e10c0a69c258081945ecd9
Arsen <developer.arsenmkhitaryan@gmail.com>
2024-08-25 20:32:59 +0400

    online store done, node testing request , postman scripting

--- JS/Node/index.js
+++ JS/Node/index.js
@@ -1,4 +1,29 @@
-import arsenstart from '../My Package in npm/index.js'
-// import arsenstart from 'arsenmkhitaryan'
+import express from 'express'
+import multer from "multer"
 
-arsenstart()
+const app = express()
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Set the destination for uploaded files
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname); // Set the filename
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+    console.log(req);
+    console.log(req.body["ars"]);
+    res.send('File uploaded successfully!');
+});
+
+app.post("/test/:id", function (req, res) {
+    const { body } = req;
+    console.log(body);
+    res.send(body ?? "hehcban")
+});
+
+app.listen(12345)
