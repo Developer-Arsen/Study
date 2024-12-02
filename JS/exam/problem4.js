5c1100b1a0b6770924a10373708ee55fcb7348ea
Arsen Mkhitaryan <developer.arsenmkhitaryan@gmail.com>
2024-12-02 13:19:07 +0400

    JS exam solving problems

---
+++ JS/exam/problem4.js
@@ -0,0 +1,6 @@
+const countFractionOnes = (number) => {
+    return number.slice(12).split('').filter(bit => bit == 1).length;;
+}
+
+console.log(countFractionOnes("1100000000011010000000000000000000000000000000000000000000000000"));
+console.log(countFractionOnes("0011111110111001100110011001100110011001100110011001100110011010"));
